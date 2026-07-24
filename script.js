01f406154a0bfb4b581c0f0c0c6862c2e53e7259
David Santos <davlexsantos@gmail.com>
2026-07-24 20:28:06 +0000

    24 de jul. de 2026, 17:28

--- script.js
+++ script.js
@@ -3,9 +3,6 @@
    ============================================= */
 const navbar = document.getElementById('navbar');
 
-window.addEventListener('scroll', () => {
-  if (window.scrollY > 30) {
-
 /* =============================================
    CERTIFICADOS - Abas simples
    ============================================= */
@@ -23,6 +20,9 @@ if (tabButtons.length) {
     });
   });
 }
+
+window.addEventListener('scroll', () => {
+  if (window.scrollY > 30) {
     navbar.classList.add('scrolled');
   } else {
     navbar.classList.remove('scrolled');
